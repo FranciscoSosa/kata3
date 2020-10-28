2fb8b0b9bceb5c1af9ff86b20a563eea6c4d86d8
dev <dev@dev-VirtualBox>
2020-10-28 12:33:51 +0000

    Kata 3 versión 2

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -2,6 +2,7 @@ package kata3;
 
 
 public class Kata3 {
+    
     public static void main(String[] args) {
         Histogram<String> histogram = new Histogram();
         histogram.increment("ull.es");
